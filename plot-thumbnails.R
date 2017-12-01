7196b3ae0df1c65b17e2fff39b32021d2b2a2afe
Peter Kamerman <peter.kamerman@gmail.com>
2017-12-01 12:26:34 +0200

    First commit: script skeleton

---
+++ plot-thumbnails.R
@@ -0,0 +1,18 @@
+############################################################
+#                                                          #
+#             Thumbnails of plot types for UI              #
+#                                                          #
+############################################################
+# Load packages
+library(ggplot2)
+
+# Histogram
+
+
+
+# Boxplot
+
+
+
+# Scatterplot
+
