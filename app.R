0561d6eaf1e4b9807d392fdf59f0694a989f525a
Kavilan Nair <kavilann@gmail.com>
2017-12-12 09:02:15 +0200

    Radio buttons to choose desired plot

--- app.R
+++ app.R
@@ -31,29 +31,23 @@ ui <- fluidPage(
                                                     `Blank space` = " ")),
                            em("(Max file size is 5MB)")),
           
-          conditionalPanel(condition = "input.tabSelected==3", sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30))
+          conditionalPanel(condition = "input.tabSelected==3",
+                           radioButtons("graphType", "Choose Plotlolo", 
+                                        selected = character(0),
+                                        choices = c("Scatter Plot" = "scatter", 
+                                                    "Histogram" = "histo", 
+                                                    "Box Plot" = "/t" )))
       ),
       
       # Show relevant ouput for each tab
       mainPanel(
           tabsetPanel(type = "tab",
                       tabPanel("About", value=1, conditionalPanel(condition = "input.choice==1"),
-p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus orci vitae erat commodo tincidunt. 
-                               Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque lobortis, 
-                               metus sit amet volutpat mollis, velit ex lobortis ligula, non hendrerit risus nulla eu risus. Praesent et ultrices orci,
-                               et molestie sem. Suspendisse elementum mauris eleifend dolor vestibulum interdum. Aenean mauris odio, efficitur in libero in,
-                               aliquam venenatis metus. Nunc at accumsan metus. Donec non dui vitae nulla sodales lobortis at sit amet quam. 
-                               Duis pellentesque odio a porta posuere."),
-p("Quisque urna libero, dapibus sed nibh pretium, congue vestibulum urna. Aliquam elementum nisl quis nibh porta varius.
+                        p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus orci vitae erat commodo tincidunt. 
+                               Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque lobortis."),
+                        p("Quisque urna libero, dapibus sed nibh pretium, congue vestibulum urna. Aliquam elementum nisl quis nibh porta varius.
                                Etiam malesuada, arcu nec iaculis sollicitudin, lacus purus aliquet ipsum, sit amet dapibus arcu mi vitae turpis.
-                               Nam ut est quis neque porta aliquam ut eget augue. Nulla ut semper dui. Donec luctus lectus ut risus consequat,
-                               pharetra lobortis risus efficitur. Aliquam vitae ipsum malesuada, rhoncus sem et, pellentesque erat. 
-                               Duis sed sem vel odio fringilla eleifend ut sit amet ex. Fusce eleifend, odio eu faucibus iaculis, 
-                                    mi ligula fermentum velit, sit amet lacinia arcu mi at risus. Nam vitae tristique nibh.")),
+                               Nam ut est quis neque porta aliquam ut eget augue.")),
                       
                       tabPanel("Data Upload", value=2,
                                conditionalPanel(condition = "input.choice==2"),
